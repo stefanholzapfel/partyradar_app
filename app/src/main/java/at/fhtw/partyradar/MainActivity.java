3b2c557c95f158899ffbe585d29a6928073b7f17
levithanc <wi12b074@technikum-wien.at>
2015-01-10 11:51:49 +0100

    Fixed few warnings and hardcoded strings

--- app/src/main/java/at/fhtw/partyradar/MainActivity.java
+++ app/src/main/java/at/fhtw/partyradar/MainActivity.java
@@ -12,7 +12,6 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.content.CursorLoader;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
